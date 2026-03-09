ea61b3dd646cbf89dc718bd9ac09eba4a6ae7829
unknown <kumawataashu09@gmail.com>
2026-03-09 17:12:47 +0530

    changed code

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.send("Server is running on root");
 })
-app.get("/test", (req, res) => {
-    res.send("Testing /test root ITs Response OK");
-})
+
 app.listen(port, () => {
   console.log(`Server (Master) running on port ${port}`);
 });

a6b0ca8b4a6a5d1bc9b2882b97f3409bc886be03
unknown <kumawataashu09@gmail.com>
2026-03-09 17:19:23 +0530

    add /test route

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.send("Server is running on root");
 })
-
+app.get("/test", (req, res) => {
+    res.send("You are testing a /test router")
+})
 app.listen(port, () => {
   console.log(`Server (Master) running on port ${port}`);
 });

8c3411c37020471d74a3f1ad74602bb5a4f4b569
Aashu09Kumawat <144932565+Aashu09Kumawat@users.noreply.github.com>
2026-03-09 17:14:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const port = 3000;
 app.get("/", (req, res) => {
     res.send("Server is running on root");
 })
-app.get("/test", (req, res) => {
-    res.send("Testing /test root ITs OK");
-})
 app.listen(port, () => {
   console.log(`Server (Master) running on port ${port}`);
 });

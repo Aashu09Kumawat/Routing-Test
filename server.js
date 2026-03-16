2c2e629c386c6408080e5e3bb4e76825b4f3656b
Aashu09Kumawat <144932565+Aashu09Kumawat@users.noreply.github.com>
2026-03-16 18:16:10 +0530

    Update server port and add /test route
    Changed server port to 3000 and added /test route.

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 5726;
+const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Server is running on root");
 })
-app.listen(port, () => {
+app.get("/test", (req, res) => {
+    res.send("You are testing a /test router")
+})
+const HOST = "0.0.0.0";   //  Force IPv4 binding
+
+app.listen(port, HOST, () => {
   console.log(`Server (Master) running on port ${port}`);
 });
